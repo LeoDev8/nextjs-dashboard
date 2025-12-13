e2756344b14484bafbda0c67fa49ef16f0959220
Leo <zhihao_2023@613.com>
2025-12-13 19:31:15 +0800

    Finish Chapter3

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, New_Amsterdam } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] })
+export const new_amsterdam = New_Amsterdam({weight: ['400'], subsets: ['latin']})
+export const lusitana = Lusitana({weight: ['400'], subsets: ['latin'] })
